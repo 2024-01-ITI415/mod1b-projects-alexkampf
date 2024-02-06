eef0345d8dbceaa77d9535189e4435b9cefcd9ad
alexkampf <alexk764@gmail.com>
2024-02-06 06:59:59 -0500

    Score Counter & adding points
    Score counters created, points added when an apple collides with basket via basket script.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    private void Start()
+    {
+        //Find reference to ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        //Get text component
+        scoreGT = scoreGO.GetComponent<Text>();
+        //set starting points to 0
+        scoreGT.text = "0";
+    }
     // Update is called once per frame
     void Update()
     {
@@ -26,5 +39,12 @@ public class Basket : MonoBehaviour
         if( collidedWith.tag == "Apple") {
             Destroy(collidedWith );
         }
+
+        //Parse text of scoreGT to Int
+        int score = int.Parse(scoreGT.text);
+        //add points for each collided apple
+        score += 100;
+        //convert score back to string and display
+        scoreGT.text = score.ToString();
     }
 }
