d1f0d128584a2978096d0445d50be416184f2766
alexkampf <alexk764@gmail.com>
2024-02-06 04:48:40 -0500

    AppleTree Basic Movement
    Basic Movement added to the AppleTree script

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -31,6 +31,21 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
         //changing direction
+        if(pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);//Move Right
+        } else if(pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections) {
+            speed *= -1; //change direction
+        }
     }
 }
