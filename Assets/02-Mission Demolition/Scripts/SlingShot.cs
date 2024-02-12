b57d51bae01dbe69baaa49117f8129193c8e6f15
alexkampf <alexk764@gmail.com>
2024-02-11 19:10:06 -0500

    Launchpoint halo,  Slingshot script modified
    Halo for Launchpoint created, modified SlingShot script to instantiate a projectile on a mouse click within the launchpoint.

--- Assets/02-Mission Demolition/Scripts/SlingShot.cs
+++ Assets/02-Mission Demolition/Scripts/SlingShot.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class SlingShot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
     void OnMouseEnter()
     {
-        print("Slingshot:OnMouseEnter()");
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit()
     {
-        print("Slingshot:OnMouseExit()");  
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        //Player has mouse button pressed down over slingshot
+        aimingMode = true;
+        //instantiate projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //Start at launchPoint
+        projectile.transform.position = launchPos;
+        //Set to kinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
